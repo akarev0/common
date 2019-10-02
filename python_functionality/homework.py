2ea57b7571c7253e6acf430eb58372328262eb4e
Yevhen <sulfur32066@gmail.com>
2019-10-02 22:40:46 +0300

    all tests OK

--- python_functionality/homework.py
+++ python_functionality/homework.py
@@ -58,7 +58,7 @@ def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     """
     Find the longest string
     """
-    return min([str(i) for i in data], key=len) if data else None
+пше
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
