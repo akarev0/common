b49a2ff2d3ff2f791a81d841fdc6bd467f8a7199
akarev0 <sulfur32066@gmail.com>
2019-10-27 16:50:07 +0200

    Actualy done!

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 from flask_lesson_intro.utils import get_data
 
 
@@ -8,37 +7,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_home_page():
-    return render_template("home.html", data=get_data())
+    return render_template("home.html")
 
 
 @app.route('/alarm_clock')
 def alarm_clock_page():
-    return render_template('alarm_clock.html')
+    return render_template('alarm_clock.html', data=get_data())
 
 
 @app.route('/headphones')
 def headphones_page():
-    return render_template('headphones.html')
+    return render_template('headphones.html', data=get_data())
 
 
 @app.route('/ipod')
 def ipod_page():
-    return render_template('ipod.html')
+    return render_template('ipod.html', data=get_data())
 
 
 @app.route('/calculator')
 def calculator_page():
-    return render_template('calculator.html')
+    return render_template('calculator.html', data=get_data())
 
 
 @app.route('/coffeemaker')
 def coffeemaker_page():
-    return render_template('coffeemaker.html')
+    return render_template('coffeemaker.html', data=get_data())
 
 
 @app.route('/battery_charger')
 def battery_charger_page():
-    return render_template('battery_charger.html')
+    return render_template('battery_charger.html', data=get_data())
+
+
+@app.route('/author')
+def author_page():
+    return render_template('author.html')
 
 
 if __name__ == "__main__":
