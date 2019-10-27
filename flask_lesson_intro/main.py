59672f1890e544f18401d4136f1cca09e0c0e2b6
akarev0 <sulfur32066@gmail.com>
2019-10-27 10:22:16 +0200

    replace the links on wikipedia to the links of your own templates pages that are related to the pictures

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-from utils import get_data
+
+from flask_lesson_intro.utils import get_data
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,36 @@ app = Flask(__name__)
 @app.route('/')
 def get_home_page():
     return render_template("home.html", data=get_data())
+
+
+@app.route('/alarm_clock')
+def alarm_clock_page():
+    return render_template('alarm_clock.html')
+
+
+@app.route('/headphones')
+def headphones_page():
+    return render_template('headphones.html')
+
+
+@app.route('/ipod')
+def ipod_page():
+    return render_template('ipod.html')
+
+
+@app.route('/calculator')
+def calculator_page():
+    return render_template('calculator.html')
+
+
+@app.route('/coffeemaker')
+def coffeemaker_page():
+    return render_template('coffeemaker.html')
+
+
+@app.route('/battery_charger')
+def battery_charger_page():
+    return render_template('battery_charger.html')
 
 
 if __name__ == "__main__":
