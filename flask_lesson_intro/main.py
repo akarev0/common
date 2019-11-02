520b40533c6a14129846547212fa2d73224ee48f
akarev0 <sulfur32066@gmail.com>
2019-11-02 17:09:53 +0200

    Fixed support comments

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -11,39 +11,12 @@ def get_home_page():
     return render_template("home.html")
 
 
-@app.route('/alarm_clock')
-def alarm_clock_page():
-    return render_template('alarm_clock.html', data=get_data())
-
-
-@app.route('/headphones')
-def headphones_page():
-    return render_template('headphones.html', data=get_data())
-
-
-@app.route('/ipod')
-def ipod_page():
-    return render_template('ipod.html', data=get_data())
-
-
-@app.route('/calculator')
-def calculator_page():
-    return render_template('calculator.html', data=get_data())
-
-
-@app.route('/coffeemaker')
-def coffeemaker_page():
-    return render_template('coffeemaker.html', data=get_data())
-
-
-@app.route('/battery_charger')
-def battery_charger_page():
-    return render_template('battery_charger.html', data=get_data())
-
-
-@app.route('/author')
-def author_page():
-    return render_template('author.html')
+@app.route('/<value>')
+def return_any_route(value):
+    for pages in get_data():
+        if pages.get('title') == value:
+            content = pages.get('text')
+            return render_template("any.html", value=value, data=content)
 
 
 if __name__ == "__main__":
