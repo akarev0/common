c6ff6180ea30773e408c8dfc35a2be927375ba18
akarev0 <sulfur32066@gmail.com>
2019-12-14 11:52:43 +0200

    Denis please check it thanks a lot

--- Books_Exchange/models/models.py
+++ Books_Exchange/models/models.py
@@ -3,18 +3,26 @@ from db import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    name = db.Column(db.String, primary_key=True)
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-    library = db.Column(db.String, nullable=False)
 
 
 class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     author = db.Column(db.String, nullable=False)
-    name = db.Column(db.String, primary_key=True)
+    book_title = db.Column(db.String, nullable=False, unique=True)
     edition = db.Column(db.String, nullable=False)
     edition_year = db.Column(db.String, nullable=False)
-    translation = db.Column(db.String)
+    translation = db.Column(db.String, nullable=True)
+    book_owner = db.Column(db.String, db.ForeignKey('users.id'))
 
 
 class Library(db.Model):
-    pass
+    __tablename__ = "library"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    book = db.Column(db.String, db.ForeignKey('books.name'))
