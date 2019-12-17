e5bea21aa643c8007841d786b60f95b2c69381bd
akarev0 <sulfur32066@gmail.com>
2019-12-17 21:29:16 +0200

    checkout to homework

--- Books_Exchange/models/models.py
+++ Books_Exchange/models/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
 
 
 class Book(db.Model):
@@ -18,11 +19,11 @@ class Book(db.Model):
     edition = db.Column(db.String, nullable=False)
     edition_year = db.Column(db.String, nullable=False)
     translation = db.Column(db.String, nullable=True)
-    book_owner = db.Column(db.String, db.ForeignKey('users.id'))
+    book_owner = db.relationship('User', backref='books')
 
 
 class Library(db.Model):
     __tablename__ = "library"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    book = db.Column(db.String, db.ForeignKey('books.name'))
+    book = db.Column(db.String, db.ForeignKey('books.book_title'))
